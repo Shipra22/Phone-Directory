c1f8473ab74a0fff828798409c476374fd0a6a6d
Shipra22 <shiprarai.lko@gmail.com>
2020-12-31 16:55:57 +0530

    Created ‘Header’ Component

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+//function component
+const Header = function() {
+   return (
+       <div className="header">
+         Phone Directory
+       </div>
+   )
+}
+export default Header;
