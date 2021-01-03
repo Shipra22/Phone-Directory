0ece27358f8ca22e5ec8897d5be4dacf2c1aa712
Shipra22 <shiprarai.lko@gmail.com>
2021-01-03 12:34:13 +0530

    Passed heading to Header component as props

--- src/App.js
+++ src/App.js
@@ -8,23 +8,28 @@ class App extends Component {
       { id:1, name: "Shipra", phone: "2222222222" },
       { id:2, name: "Ayushi", phone: "99999999999" },
     ];
+
     return (
       <div >
-        <Header />
+        <Header name="Phone-Directory"  />
+       
         <div className="container">
+       
           <button className="add-btn">Add</button>
           <div className="grid-container heading-container">
-            <span className="grid-item name-heading">Name</span><br />
+            <span className="grid-item name-heading">Name</span>
             <span className="grid-item phone-heading">Phone</span>
           </div>
 
           {subscribers.map(sub => {
             return <div key={sub.id} className="grid-container">
-              <span className="grid-item ">{sub.name}</span><br />
+              <span className="grid-item ">{sub.name}</span>
               <span className="grid-item ">{sub.phone}</span>
+              <button className=" del-btn" >Delete</button>
             </div>
           })
           }
+         
         </div>
 
       </div>
